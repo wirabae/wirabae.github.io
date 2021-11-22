f932b4eb5987fb4957572f0ce3b37031dfb0b764
Wira Bae <47467852+wirabae@users.noreply.github.com>
2021-11-22 23:56:07 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+jQuery(document).ready(function() {	
+	
+    /*
+        Background slideshow
+    */
+	$('.banner-area').backstretch([
+	                     "images/backgrounds/1.jpg"
+	                   , "images/backgrounds/2.jpg"
+	                   , "images/backgrounds/3.jpg"
+	                  ], {duration: 3000, fade: 750});
+					  
+    
+  
+
+});
+
+	// cowntdown function. Set the date by modifying the date in next line (June 1, 2012 00:00:00):
+	var austDay = new Date("December 31, 2018 00:00:00");
+		$('#countdown').countdown({until: austDay, layout: '<div class="item"><p>{dn}</p> {dl}</div> <div class="item"><p>{hn}</p> {hl}</div> <div class="item"><p>{mn}</p> {ml}</div> <div class="item"><p>{sn}</p> {sl}</div>'});
+		$('#year').text(austDay.getFullYear());
+		
+	// smooth scrolling	
+		$(function() {
+	  $('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	});
+	
