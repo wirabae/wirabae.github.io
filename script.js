207202fb264d5ed287d6e95b4d92697a9b9d3004
Wira Bae <47467852+wirabae@users.noreply.github.com>
2022-10-17 17:14:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.addEventListener('load', () => {
     let totalSeconds;
     
     function init() {
-        totalSeconds = Math.floor((new Date('01/01/2023') - new Date()) / 1000); 
+        totalSeconds = Math.floor((new Date('14/04/2023') - new Date()) / 1000); 
         setTimeLeft();
         let interval = setInterval(() => {
             if (totalSeconds < 0) {
