6149111d319912d68c5359a0abc66f4f1c90e414
Wira Bae <47467852+wirabae@users.noreply.github.com>
2023-12-22 00:11:28 +0700

    Update script.js

--- passgen2/script.js
+++ passgen2/script.js
@@ -1,106 +1,33 @@
-const lengthSlider = document.querySelector(".pass-length input");
-const options = document.querySelectorAll(".option input");
-const copyIcon = document.querySelector(".input-box span");
-const passwordInput = document.querySelector(".input-box input");
-const passIndicator = document.querySelector(".pass-indicator");
-const generateBtn = document.querySelector(".generate-btn");
-
-const characters = {
-    lowercase: "abcdefghijklmnopqrstuvwxyz",
-    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    numbers: "0123456789",
-    symbols: "!$%&|[](){}:;.,*+-#@<>~"
-}
-
-const generatePassword = () => {
-    let staticPassword = "",
-        randomPassword = "",
-        excludeDuplicate = false,
-        passLength = lengthSlider.value;
-
-    options.forEach(option => {
-        if (option.checked) {
-            if (option.id !== "exc-duplicate" && option.id !== "spaces") {
-                staticPassword += characters[option.id];
-            } else if (option.id === "spaces") {
-                staticPassword += `  ${staticPassword}  `;
-            } else {
-                excludeDuplicate = true;
-            }
-        }
-    });
-
-    for (let i = 0; i < passLength; i++) {
-        let randomChar = staticPassword[Math.floor(Math.random() * staticPassword.length)];
-        if (excludeDuplicate) {
-            !randomPassword.includes(randomChar) || randomChar == " " ? randomPassword += randomChar : i--;
+const lengthSlider = document.querySelector(".pass-length input"); const options = document.querySelectorAll(".option input"); const copyIcon = document.querySelector(".input-box span"); const passwordInput = document.querySelector(".input-box input"); const passIndicator = document.querySelector(".pass-indicator"); const generateBtn = document.querySelector(".generate-btn");const characters = { lowercase: "abcdefghijklmnopqrstuvwxyz", uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ", numbers: "0123456789", // symbols: "!$%&|{}:;.,+-#@<>~" symbols: "!@#$%^&()_+-={}[]|:;'<>,.?/" }const generatePassword = () => { let staticPassword = "", randomPassword = "", excludeDuplicate = false, passLength = lengthSlider.value;options.forEach(option => {
+    if (option.checked) {
+        if (option.id !== "exc-duplicate" && option.id !== "spaces") {
+            staticPassword += characters[option.id];
+        } else if (option.id === "spaces") {
+            staticPassword += `  ${staticPassword}  `;
         } else {
-            randomPassword += randomChar;
+            excludeDuplicate = true;
         }
     }
-    passwordInput.value = randomPassword;
-
-}
-
-const updatePassIndicator = () => {
-    passIndicator.id = lengthSlider.value <= 8 ? "weak" : lengthSlider.value <= 16 ? "medium" : "strong";
-}
-
-const updateSlider = () => {
-    document.querySelector(".pass-length span").innerText = lengthSlider.value;
-    generatePassword();
-    updatePassIndicator();
-}
-updateSlider();
-
-const copyPassword = () => {
-    navigator.clipboard.writeText(passwordInput.value);
-    copyIcon.innerText = "check";
-    copyIcon.style.color = "#4285f4";
-    setTimeout(() => {
-        copyIcon.innerText = "copy_all";
-        copyIcon.style.color = "#707070";
-    }, 1500);
-}
-
-
-
-easyToSayRadio.addEventListener('change', function () {
-    if (this.checked) {
-        uppercaseCheckbox.checked = true;
-        lowercaseCheckbox.checked = true;
-
-        numbersCheckbox.checked = false;
-        symbolsCheckbox.checked = false;
-
-        numbersCheckbox.disabled = true;
-        symbolsCheckbox.disabled = true;
-    }
 });
 
-easyToReadRadio.addEventListener('change', function () {
-    if (this.checked) {
-        uppercaseCheckbox.checked = true;
-        lowercaseCheckbox.checked = true;
-
-        numbersCheckbox.checked = false;
-        symbolsCheckbox.checked = false;
-
-        numbersCheckbox.disabled = false;
-        symbolsCheckbox.disabled = false;
+for (let i = 0; i < passLength; i++) {
+    let randomChar = staticPassword[Math.floor(Math.random() * staticPassword.length)];
+    if (excludeDuplicate) {
+        !randomPassword.includes(randomChar) || randomChar == " " ? randomPassword += randomChar : i--;
+    } else {
+        randomPassword += randomChar;
     }
-});
+}
+passwordInput.value = randomPassword;}const updatePassIndicator = () => { passIndicator.id = lengthSlider.value <= 8 ? "weak" : lengthSlider.value <= 16 ? "medium" : "strong"; }const updateSlider = () => { document.querySelector(".pass-length span").innerText = lengthSlider.value; generatePassword(); updatePassIndicator(); } updateSlider();const copyPassword = () => { navigator.clipboard.writeText(passwordInput.value); copyIcon.innerText = "check"; copyIcon.style.color = "#4285f4"; setTimeout(() => { copyIcon.innerText = "copy_all"; copyIcon.style.color = "#707070"; }, 1500); }const easyToSayRadio = document.getElementById('easy-to-say'); const easyToReadRadio = document.getElementById('easy-to-read'); const allCharactersRadio = document.getElementById('all-characters');const uppercaseCheckbox = document.getElementById('uppercase'); const lowercaseCheckbox = document.getElementById('lowercase'); const numbersCheckbox = document.getElementById('numbers'); const symbolsCheckbox = document.getElementById('symbols');easyToSayRadio.addEventListener('change', function () { if (this.checked) { uppercaseCheckbox.checked = true; lowercaseCheckbox.checked = true;numbersCheckbox.checked = false;
+    symbolsCheckbox.checked = false;
 
-allCharactersRadio.addEventListener('change', function () {
-    if (this.checked) {
-        uppercaseCheckbox.checked = true;
-        lowercaseCheckbox.checked = true;
-        numbersCheckbox.checked = true;
-        symbolsCheckbox.checked = true;
+    numbersCheckbox.disabled = true;
+    symbolsCheckbox.disabled = true;
+}});easyToReadRadio.addEventListener('change', function () { if (this.checked) { uppercaseCheckbox.checked = true; lowercaseCheckbox.checked = true;numbersCheckbox.checked = false;
+    symbolsCheckbox.checked = false;
 
-        numbersCheckbox.disabled = false;
-        symbolsCheckbox.disabled = false;
-    }
-});copyIcon.addEventListener("click", copyPassword);
-lengthSlider.addEventListener("input", updateSlider);
-generateBtn.addEventListener("click", generatePassword);
+    numbersCheckbox.disabled = false;
+    symbolsCheckbox.disabled = false;
+}});allCharactersRadio.addEventListener('change', function () { if (this.checked) { uppercaseCheckbox.checked = true; lowercaseCheckbox.checked = true; numbersCheckbox.checked = true; symbolsCheckbox.checked = true;numbersCheckbox.disabled = false;
+    symbolsCheckbox.disabled = false;
+}});copyIcon.addEventListener("click", copyPassword); lengthSlider.addEventListener("input", updateSlider); generateBtn.addEventListener("click", generatePassword);
